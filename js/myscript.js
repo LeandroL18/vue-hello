a026dc401ffbb1378f5a31ae0c8d9f0fee4f811a
LeandroL18 <leandrolipani6@gmail.com>
2023-12-11 10:19:02 +0100

    Bonus

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,15 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
-const { createApp, ref } = Vue
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue
 
   createApp({
     data() {
       return {
         messaggio: "VUE HELLO!",
+        src: "img/01.webp"
       }
     }
   }).mount('#app')
